4d1730aedb8c5086baaba932d4e57df0ca243991
Yolanda Zhi <yolandazik@qq.com>
2025-02-21 23:44:38 -0500

    init flask app with redis and graphql schema

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_graphql import GraphQLView
+from flask_cors import CORS  # CORS
+from schema import schema
+
+app = Flask(__name__)
+CORS(app)  # Allow CORS
+
+# GraphQL endpoint
+app.add_url_rule(
+    "/graphql",
+    view_func=GraphQLView.as_view("graphql", schema=schema, graphiql=True)
+)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
