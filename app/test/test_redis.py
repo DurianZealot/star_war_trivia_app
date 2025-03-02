db3255bfb335f5105f6f31c68fda97a29f12c4cc
Yolanda Zhi <yolandazik@qq.com>
2025-03-01 21:03:52 -0500

    add unit tests for redis

---
+++ app/test/test_redis.py
@@ -0,0 +1,25 @@
+from redis_client import get_search, save_search
+import pytest
+
+import sys
+import os
+sys.path.append(os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '../')))
+
+
+def test_redis_save_command():
+    key = "TEST_GET"
+    expected_value = "EXPECT_GET_VAL"
+
+    response = save_search(key, expected_value)
+
+    assert response == True
+
+
+def test_redis_get_command():
+    key = "TEST_GET"
+    expected_value = "EXPECT_GET_VAL"
+
+    response = get_search(key)
+
+    assert response == expected_value
