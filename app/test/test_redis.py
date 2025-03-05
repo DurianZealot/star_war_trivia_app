d1ba98450d2f3a01bb8b5c3ff0ad59b2d106272b
Yolanda Zhi <yolandazik@qq.com>
2025-03-04 23:30:25 -0500

    make redis_client into a class

--- app/test/test_redis.py
+++ app/test/test_redis.py
@@ -3,15 +3,17 @@ import os
 sys.path.append(os.path.abspath(
     os.path.join(os.path.dirname(__file__), '../')))
 
-from redis_client import get_search, save_search
+from redis_client import RedisClient
 import pytest
 
+# Init a RedisClient to connect to Redis 
+RedisClientTestInstance = RedisClient("redis", 6379)
 
 def test_redis_save_command():
     key = "TEST_GET"
     expected_value = "EXPECT_GET_VAL"
 
-    response = save_search(key, expected_value)
+    response = RedisClientTestInstance.save_search(key, expected_value)
 
     assert response == True
 
@@ -20,6 +22,6 @@ def test_redis_get_command():
     key = "TEST_GET"
     expected_value = "EXPECT_GET_VAL"
 
-    response = get_search(key)
+    response = RedisClientTestInstance.get_search(key)
 
     assert response == expected_value
